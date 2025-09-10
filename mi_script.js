115aab2e90cca6bced415a433af32e63efc6ee62
julio00004 <robinjulio004@gmail.com>
2025-09-09 19:24:06 -0600

    Update mi_script.js

--- mi_script.js
+++ mi_script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 AFRAME.registerComponent('clipping-plane', {
       schema: {
         dir: { type: 'vec3', default: { x: 0, y: 0, z: 0 } },
@@ -98,44 +99,4 @@ AFRAME.registerComponent('mi_ccplane2',{
     this.el.object3D.add(planeHelper);
   }
 });
-
-const myCheckbox = document.getElementById('button6');
-const entidad = document.querySelector('#modelo3d');
-
-document.getElementById('boton1').addEventListener('click', function() {
-  entidad.setAttribute('clipping-plane', 'enabled', 'false');
 });
-
-document.getElementById('boton2').addEventListener('click', function() {
-  entidad.setAttribute('clipping-plane', 'enabled', 'true');
-});
-
-document.getElementById('boton3').addEventListener('click', function() {
- entidad.setAttribute('clipping-plane', {dir: {x: 0, y: -0.22, z: 0},enabled:'true'});
-});
-
-document.getElementById('boton4').addEventListener('click', function() {
- entidad.setAttribute('clipping-plane', {dir: {x: -1, y: 0, z: 0},enabled:'true'});
-});
-
-document.getElementById('boton5').addEventListener('click', function() {
- entidad.setAttribute('clipping-plane', {dir: {x: 0, y: 0, z: -1},enabled:'true'});
-});
-
-myCheckbox.addEventListener('change', function() {
-  const ldir= entidad.components['clipping-plane'].data.dir;//solo lectura
-  const plane1 = document.querySelector('#plano1');
-  const plane2 = document.querySelector('#plano2');
-  const plane3 = document.querySelector('#plano3');
-          plane1.setAttribute('visible', 'false');
-          plane2.setAttribute('visible', 'false');
-          plane3.setAttribute('visible', 'false'); 
-       if (this.checked) {
-         if(ldir.z==-1){
-              plane1.setAttribute('visible','true');
-           }else if(ldir.x==-1)
-             {
-              plane2.setAttribute('visible','true');
-             }else if(ldir.y==-0.22){
-              plane3.setAttribute('visible','true');
-        }}});
